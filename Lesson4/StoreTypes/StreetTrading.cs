479c93bb672babc182c12872f7fe6b63693fb965
Tsimafei <tima.kutsko@gmail.com>
2022-12-18 21:41:27 +0300

    Lesson4. Решение дз

---
+++ Lesson4/StoreTypes/StreetTrading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson4.StoreTypes
+{
+    internal class StreetTrading
+    {
+        public List<StreetTradingFruit> ProductFruits { get; set; }
+
+        public List<StreetTradingVegetable> ProductVegetables { get; set; }
+    }
+
+    internal class StreetTradingEntity
+    {
+        public string Name { get; set; }
+
+        public int Cost { get; set; }
+    }
+
+    internal class StreetTradingFruit : StreetTradingEntity
+    {
+    }
+
+    internal class StreetTradingVegetable : StreetTradingEntity
+    {
+    }
+}
